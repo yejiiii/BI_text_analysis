66b35c981705f4cb567523d9c7a80a09d9b864ec
YehoonJang <jangyh0420@gmail.com>
2017-06-12 13:40:19 +0900

    ,

--- bi_project/bi/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ReviewWrite
-
-# Register your models here.
-
-class ReviewWriteAdmin(admin.ModelAdmin):
-    list_diplay = ('review_data',)
-
-admin.site.register(ReviewWrite, ReviewWriteAdmin)
--- bi_project/bi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BiConfig(AppConfig):
-    name = 'bi'
